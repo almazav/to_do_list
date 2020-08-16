8cad0b3c32e382bd51d311d458765972fe4f5474
WeronikaMicyk <alemazav1002@gmail.com>
2020-08-16 18:04:59 +0100

    firrst commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,94 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
+import { BrowserRouter as Router, Route} from 'react-router-dom';
 import './App.css';
+import Todos from './components/Todos';
+import Header from './layout/Header1';
+import AddToDo from './components/AddToDo';
+import About from './components/pages/about';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+// import { v4 as uuidv4 } from 'uuid';
+import axios from 'axios';
+//class App extends React.Component (other way to do it whitout importing components)
+class App extends Component {
+   state={
+     todos: [
+    //    {
+    //   id: uuidv4(), 
+    //   title:'Take out the trash',
+    //   completed:false
+    //  },
+    //  {
+    //   id: uuidv4(), 
+    //   title:'Dinner with bru',
+    //   completed:false
+    //  },
+    //  {
+    //   id: uuidv4(), 
+    //   title:'Take roby out',
+    //   completed:false
+    //  }
+
+    ]
+
+     
+
+   }
+
+  componentDidMount() {
+    axios.get('https://jsonplaceholder.typicode.com/todos?_limit=10').then(res => this.setState({ todos: res.data}))
+  }
+// Toggle Complete
+   markComplete = (id) =>{
+    this.setState({todos: this.state.todos.map(todo => {
+      if(todo.id === id){
+        todo.completed =!todo.completed
+      }
+      return todo;
+    })})
 }
 
+//delete todo
+delTodo = (id) =>{
+  axios.delete(`https://jsonplaceholder.typicode.com/todos/${id}`)
+    .then(res=>   this.setState({todos:[...this.state.todos.filter(todo => todo.id !== id)]})
+    );
+}
+//Add Todo
+
+addTodo  = (title) => {
+
+   axios.post('https://jsonplaceholder.typicode.com/todos',{
+     title,
+     completed: false
+   })
+    .then(res=> this.setState({ todos: [...this.state.todos, res.data]}));
+  
+}
+
+
+render() {              // this is the only required life cycle method tis render the web app
+  return (       
+    <Router>       
+    <div className="App">  
+      <div className = 'container'>
+        <Header/>
+        <Route exact path='/' render={props =>(
+          <React.Fragment>
+            <AddToDo addTodo = {this.addTodo}/>
+            <Todos todos ={this.state.todos} markComplete={this.markComplete} delTodo ={this.delTodo}/>
+
+          </React.Fragment>
+        )}/>
+        <Route path ="/about" component={About}/>
+      </div>
+    </div>
+    </Router> 
+  );
+}}
 export default App;
+
+// STATES 
+// diferent components need state 
+// states are added within the class
+// to acces state you can do this.state.todos
+//taking the todos state and passing to the components as a prop
---
+++ src/components/pages/about.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+ function About() {
+    return (
+      <React.Fragment>
+          <h1>About</h1>
+          <p>todolist app v1.0 reat crash course </p>
+
+      </React.Fragment>
+    )
+}
+
+export default About;
